6d0326f0c0eb1f9d99c4a895839d1fef069e6802
thsowers <tyler@startrakstudios.com>
2019-02-16 00:08:39 -0500

    Basic search functionality

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate clap;
 
 use std::fs::File;
 use serde_json::Value;
@@ -18,13 +20,34 @@ struct Poems {
 #[derive(Serialize, Deserialize, Debug)]
 struct Poem {
     author: String,
-    reference: String,
     title: String,
     text: Vec<String>,
 }
 
+
 fn main() {
-    let mut poem_vec: Vec<&Poem> = Vec::new();
+    let matches = clap_app!(lyrics =>
+        (version: "0.1.0")
+        (author: "Tyler H. Sowers <thsowers@gmail.com>")
+        (about: "Search Poems")
+        (@arg AUTHOR: +required "Author of poem")
+        (@arg TITLE: +required "Title of poem")
+    ).get_matches();
+
+    // Get args
+    let author = str::replace(matches.value_of("AUTHOR").unwrap(), "_", " ");
+    let title = str::replace(matches.value_of("TITLE").unwrap(), "_", " ");
+
+    let poems = get_poems();
+    println!("{}, {}", author, title);
+
+    let res = poems.iter().find(|x| x.title == title && x.author == author);
+
+    println!("{}", serde_json::to_string_pretty(&res).unwrap());
+}
+
+fn get_poems() -> Vec<Poem> {
+    let mut poem_vec: Vec<Poem> = Vec::new();
 
     let mut s = String::new();
     let mut data = File::open("poems.json").unwrap().read_to_string(&mut s).unwrap();
@@ -32,9 +55,10 @@ fn main() {
         serde_json::from_str(&s).expect("error while reading json");
 
 
-    for (i, poem) in deserialized_poems.poems.iter().enumerate() {
-        poem_vec.insert(i, poem);
+    for poem in deserialized_poems.poems {
+        poem_vec.push(poem);
     }
-    println!("Finished adding poem_vec");
-    println!("{}", poem_vec.len());
+    println!("Loaded {} poems ready for search", poem_vec.len());
+
+    poem_vec
 }
